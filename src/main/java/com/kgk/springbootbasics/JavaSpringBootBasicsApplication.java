d605ef9aef234eec837c51da1be5fb48bfcdb781
Kalani <kalani.gayathri91@gmail.com>
2022-07-23 05:35:12 -0700

    Spring Boot Basics Learning Project

---
+++ src/main/java/com/kgk/springbootbasics/JavaSpringBootBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.kgk.springbootbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootBasicsApplication.class, args);
+	}
+
+}
